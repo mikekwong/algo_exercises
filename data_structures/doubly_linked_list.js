2dbb8b3ffb93451ec38fdeff1fa28e89e43a2c18
Mike Wong <wongmdesign@gmail.com>
2019-12-14 22:41:04 -0500

    push added

--- data_structures/doubly_linked_list.js
+++ data_structures/doubly_linked_list.js
@@ -12,6 +12,22 @@ class DoublyLinkedList {
     this.tail = null
     this.length = 0
   }
+
+  push (val) {
+    let newNode = new Node(val)
+    if (this.length === 0) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      // point the new node val's previous to the old tail
+      newNode.prev = this.tail
+      // update the new current tail to be new node val
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
 }
 
 let first = new Node(12)
